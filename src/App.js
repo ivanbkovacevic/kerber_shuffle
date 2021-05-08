4090639a83cd71fe17d5832afbc21f5f9dedacc0
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2021-05-08 18:08:41 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import { useState } from "react";
+import { Cards } from "./Cards";
 
-function App() {
+export default function App() {
+
+  const [cardToDisplay, setCardToDisplay] = useState(Cards[0].imageUrl);
+
+  const [start, setStart] = useState("");
+ 
+
+  let tempCardNum = 0;
+  const startInterval = () => {
+    setStart(setInterval(printCard, 300));
+  };
+
+  const printCard = () => {
+    if (tempCardNum === Cards.length - 1) {
+      tempCardNum = 0;
+   
+      setCardToDisplay(Cards[tempCardNum].imageUrl);
+       console.log(Cards.length);
+    console.log(tempCardNum + "cardnum");
+    } else {
+      tempCardNum++;
+     
+      setCardToDisplay(Cards[tempCardNum].imageUrl);
+    }
+
+    console.log("printCardddd");
+    console.log(Cards.length);
+   
+  };
+
+  const stopInterval = () => {
+    clearInterval(start);
+   
+    console.log("stop");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="header"><h1>CARDS SHUFFLING APP</h1></div>
+    
+      <div className="buttons-container">
+          <button onClick={startInterval}>Click to shuffle cards</button>
+          <button onClick={stopInterval}>Stop the shuffle</button>
+      </div>
+     <div className="card-container">
+       <div className="img-cont"><img src={cardToDisplay} alt="nsto"></img></div>
+  
+     <div className="description">In Italy, pizza served in formal settings, such as at a restaurant, is presented unsliced, and is eaten with the use of a knife and fork.[2][3] In casual settings, however, it is cut into wedges to be eaten while held in the hand.
+
+.</div>
+     </div>
+     
     </div>
   );
 }
-
-export default App;
---
+++ src/Cards.js
@@ -0,0 +1,146 @@
+export const Cards=[
+    {
+    "id": 0,
+    "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg",
+    "name": "Braccio di Ferro",
+    "types": 'pizza',
+    "sizes": [26, 30, 40],
+    "price": 15,
+    "category": 0,
+    "rating": 4,
+    "qnt":0
+    },
+    {
+    "id": 1,
+    "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg",
+    "name": "Napoletana",
+    "types": 'pizza',
+    "sizes": [26, 40],
+    "price": 20,
+    "category": 1,
+    "rating": 6,
+    "qnt":0
+    },
+    {
+    "id": 2,
+    "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/6652fec1-04df-49d8-8744-232f1032c44b.jpg",
+    "name": "Carbonara",
+    "types": 'pizza',
+    "sizes": [26, 40],
+    "price": 30,
+    "category": 1,
+    "rating": 4,
+    "qnt":0
+    },
+    {
+    "id": 3,
+    "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/af553bf5-3887-4501-b88e-8f0f55229429.jpg",
+    "name": "Americana",
+    "types": 'pizza',
+    "sizes": [26, 30, 40],
+    "price": 25,
+    "category": 2,
+    "rating": 2,
+    "qnt":0
+    },
+    {
+    "id": 4,
+    "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg",
+    "name": "Quattro Stagioni",
+    "types": 'pizza',
+    "sizes": [26, 30, 40],
+    "price": 5,
+    "category": 3,
+    "rating": 8,
+    "qnt":0
+    },
+    {
+    "id": 5,
+    "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/1e1a6e80-b3ba-4a44-b6b9-beae5b1fbf27.jpg",
+    "name": "Red peperoni",
+    "types": 'pizza',
+    "sizes": [30, 40],
+    "price": 15,
+    "category": 2,
+    "rating": 2,
+    "qnt":0
+    },
+    {
+    "id": 6,
+    "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/d2e337e9-e07a-4199-9cc1-501cc44cb8f8.jpg",
+    "name": "Peperoni",
+    "types": 'pizza',
+    "sizes": [26, 30, 40],
+    "price": 20,
+    "category": 1,
+    "rating": 9,
+    "qnt":0
+    },
+    {
+    "id": 7,
+    "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/d48003cd-902c-420d-9f28-92d9dc5f73b4.jpg",
+    "name": "Margherita",
+    "types": 'pizza',
+    "sizes": [26, 30, 40],
+    "price": 10,
+    "category": 4,
+    "rating": 10,
+    "qnt":0
+    },
+    {
+    "id": 8,
+    "imageUrl": "https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/30367198-f3bd-44ed-9314-6f717960da07.jpg",
+    "name": "Vegeteriana",
+    "types": 'pizza',
+    "sizes": [26, 30, 40],
+    "price": 17,
+    "category": 5,
+    "rating": 7,
+    "qnt":0
+    },
+    {
+    "id": 9,
+    "imageUrl": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/2/28/1/FNM_040112-Copy-That-002_s4x3.jpg.rend.hgtvcom.826.620.suffix/1382541346030.jpeg",
+    "name": "Barbecue",
+    "types": 'pizza',
+    "sizes": [26, 30, 40],
+    "price": 22,
+    "category": 5,
+    "rating": 7,
+    "qnt":0
+    },
+    {
+    "id": 10,
+    "imageUrl": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/2/28/1/FNM_040112-Copy-That-002_s4x3.jpg.rend.hgtvcom.826.620.suffix/1382541346030.jpeg",
+    "name": "Coca-Cola",
+    "types": 'drinks',
+    "sizes": [26, 30, 40],
+    "price": 10,
+    "category": 4,
+    "rating": 10,
+    "qnt":0
+    },
+    {
+    "id": 11,
+    "imageUrl": "https://www.ediskont.rs/uploads/store/products/images/pepsi_025l_640.jpg",
+    "name": "Pepsi",
+    "types": 'drinks',
+    "sizes": [26, 30, 40],
+    "price": 8,
+    "category": 4,
+    "rating": 10,
+    "qnt":0
+    },
+    {
+    "id": 12,
+    "imageUrl": "https://assets.sainsburys-groceries.co.uk/gol/2900766/1/640x640.jpg",
+    "name": "Fanta",
+    "types": 'drinks',
+    "sizes": [26, 30, 40],
+    "price": 9,
+    "category": 4,
+    "rating": 10,
+    "qnt":0
+    }
+    ]
+    export default [Cards]
